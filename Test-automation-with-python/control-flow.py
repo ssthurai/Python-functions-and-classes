495eaab1eac5bcd419a56d9df0c5997b0ab13f98
ssthurai <seenithurai@gmail.com>
2023-05-16 22:16:44 +0800

    Add some examples

---
+++ Test-automation-with-python/control-flow.py
@@ -0,0 +1,16 @@
+if True:
+    print('true!')
+if False:
+    print('false!')
+
+
+a = 10
+
+if a == 5:
+    print("This is 5")
+elif a == 6: 
+    print("This is 6")
+elif a == 7:
+    print("This is 7")
+else:
+    print("This is not 5 or 6 or 7")
